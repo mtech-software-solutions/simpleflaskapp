6b7d5f2243bf6b02703358bb665466e2da49fafc
Earl <earl.gray@qa.com>
2024-09-11 10:25:36 +0100

    EG: changed a thing

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return '<h1>Changed text.</h1>'
+    return '<h1>Other text.</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
